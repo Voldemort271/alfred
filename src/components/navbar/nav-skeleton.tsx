7943142c357fc8706912ae989d8b709686d5efba
voldemort271 <shubhamspadhi@gmail.com>
2024-10-05 00:11:42 +0530

    Made nav responsive

--- src/components/navbar/nav-skeleton.tsx
+++ src/components/navbar/nav-skeleton.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { type ReactNode, useEffect, useState } from "react";
+import { MenuIcon, X } from "lucide-react";
 
 interface Props {
   children: ReactNode;
@@ -9,6 +10,7 @@ interface Props {
 
 const NavSkeleton = ({ children }: Props) => {
   const [isShrunk, setShrunk] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     const handler = () => {
@@ -35,21 +37,39 @@ const NavSkeleton = ({ children }: Props) => {
   }, []);
 
   return (
-    <nav
-      className={`flex flex-row items-center justify-between border-b-2 border-b-zinc-200 px-12 ${isShrunk ? "py-2" : "py-5"} text-base transition-all`}
-    >
-      <div
-        className={`transition-all ${isShrunk ? "text-2xl" : "text-3xl"} font-semibold uppercase`}
+    <>
+      <nav
+        className={`flex flex-row items-center justify-between border-b-2 border-b-zinc-200 px-5 sm:px-12 ${isShrunk ? "py-5 sm:py-2" : "py-5"} text-base transition-all`}
       >
-        <Link href={"/public"}>Alfred&reg;</Link>
-      </div>
-      <div className="flex flex-row items-center gap-8 font-medium uppercase">
+        <div
+          className={`transition-all ${isShrunk ? "text-2xl" : "text-2xl sm:text-3xl"} font-semibold uppercase`}
+        >
+          <Link href={"/public"}>Alfred&reg;</Link>
+        </div>
+        <div className="hidden flex-row items-center gap-8 font-medium uppercase sm:flex">
+          <Link href={"/public"}>men</Link>
+          <Link href={"/public"}>women</Link>
+          <Link href={"/public"}>kids</Link>
+          {children}
+        </div>
+        <div
+          className="flex h-full cursor-pointer items-center justify-center text-xl sm:hidden"
+          onClick={() => setToggle(!toggle)}
+        >
+          {toggle ? <X size={32} /> : <MenuIcon size={32} />}
+        </div>
+      </nav>
+      <div
+        className={`flex w-full flex-col gap-5 overflow-y-clip px-5 font-medium uppercase transition-all ${
+          toggle ? "border-b-2 border-zinc-200 py-5" : "h-0"
+        }`}
+      >
         <Link href={"/public"}>men</Link>
         <Link href={"/public"}>women</Link>
         <Link href={"/public"}>kids</Link>
         {children}
       </div>
-    </nav>
+    </>
   );
 };
 
